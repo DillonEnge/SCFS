ebb9fd34445b1f36a942e523c40c088647189da0
DillonEnge <dillon.enge@gmail.com>
2019-04-08 08:30:18 -0400

    Began refactoring trait_tree creation.

--- lib.py
+++ lib.py
@@ -1,14 +1,17 @@
 # lib.py: Lib file responsible for the heavy lifting of stat generation and json manipulation.
 
-import json, random
+import json, random, os
 from scipy.stats import truncnorm
 
 # Writes the initial default trait_tree object to a json file
-def generate_initial_json():
-    with open('trait_tree.json', 'w') as outfile:
+def generate_initial_json(name):
+    if not os.path.isdir(os.getcwd() + '/trait_trees'):
+        os.mkdir(os.getcwd() + '/trait_trees')
+    with open('trait_trees/' + name + '_trait_tree.json', 'w') as outfile:
         json.dump(
             {
-                'value': 0,
+                'name': name,
+                'value': 1,
                 'traits':
                     {
                         'agreeableness': {
@@ -69,25 +72,101 @@ def generate_initial_json():
                 }
             }, outfile, indent=4)
 
-# Gets the trait_tree object from a json file and returns it
-def get_trait_tree():
-    trait_tree = {}
-    with open('trait_tree.json') as trait_tree_file:
-        trait_tree = json.load(trait_tree_file)
+#TODO Temporary home for this class, will need to be refactored later.
+class TraitTreeModifier:
+    def __init__(self, name):
+        self.name = name
 
-    return trait_tree
+    # Writes the initial default trait_tree object to a json file
+    def generate_initial_json(self):
+        if not os.path.isdir(os.getcwd() + '/trait_trees'):
+            os.mkdir(os.getcwd() + '/trait_trees')
+        with open('trait_trees/' + self.name + '_trait_tree.json', 'w') as outfile:
+            json.dump(
+                {
+                    'name': self.name,
+                    'value': 1,
+                    'traits':
+                        {
+                            'agreeableness': {
+                                'value': 0,
+                                'subtraits': {
+                                    'trust': 0,
+                                    'morality': 0,
+                                    'altruism': 0,
+                                    'cooperation': 0,
+                                    'modesty': 0,
+                                    'sympathy': 0
+                                }
+                            },
+                            'conscientiousness': {
+                                'value': 0,
+                                'subtraits': {
+                                    'self-efficacy': 0,
+                                    'orderliness': 0,
+                                    'dutifulness': 0,
+                                    'achievement-striving': 0,
+                                    'self-discipline': 0,
+                                    'cautiousness': 0
+                                }
+                            },
+                            'extraversion': {
+                                'value': 0,
+                                'subtraits': {
+                                    'friendliness': 0,
+                                    'gregariousness': 0,
+                                    'assertiveness': 0,
+                                    'activity level': 0,
+                                    'excitement-seeking': 0,
+                                    'cheerfulness': 0
+                                }
+                            },
+                            'neuroticism': {
+                                'value': 0,
+                                'subtraits': {
+                                    'Anxiety': 0,
+                                    'Anger': 0,
+                                    'Depression': 0,
+                                    'Self-consciousness': 0,
+                                    'Immoderation': 0,
+                                    'Vulnerability': 0
+                                }
+                            },
+                            'openness': {
+                                'value': 0,
+                                'subtraits': {
+                                    'imagination': 0,
+                                    'artistic interests': 0,
+                                    'emotionality': 0,
+                                    'adventurousness': 0,
+                                    'intellect': 0,
+                                    'liberalism': 0
+                                }
+                            }
+                    }
+                }, outfile, indent=4)
 
-# Writes the trait_tree object it receives to a json file
-def write_trait_tree(trait_tree):
-    with open('trait_tree.json', 'w') as outfile:
-        json.dump(trait_tree, outfile, indent=4)
+    # Gets the trait_tree object from a json file and returns it
+    def get_trait_tree(self):
+        trait_tree = {}
+        with open('trait_trees/' + self.name + '_trait_tree.json') as trait_tree_file:
+            trait_tree = json.load(trait_tree_file)
+
+        return trait_tree
+
+    # Writes the trait_tree object it receives to a json file
+    def write_trait_tree(self, trait_tree):
+        with open('trait_trees/' + self.name + '_trait_tree.json', 'w') as outfile:
+            json.dump(trait_tree, outfile, indent=4)
 
 # Will eventually take 'age' as a param to signify max percentage remaining
-def fill_random_sub_values():
-    trait_tree = get_trait_tree()
+def fill_random_sub_values(name):
+    trait_tree_modifier = TraitTreeModifier(name)
+    trait_tree_modifier.generate_initial_json()
+    trait_tree = trait_tree_modifier.get_trait_tree()
     traits = get_and_shuffle_trait_tree(trait_tree['traits'])
 
-    iterate_value_generation(trait_tree, traits)
+    iterate_value_generation(trait_tree, traits, trait_tree_modifier=trait_tree_modifier, name=name)
 
 # Will eventually be used in the context of physical/mental attribute generation that can be estimated using a normally distributed model
 # Example usage:
@@ -113,7 +192,7 @@ def get_and_shuffle_trait_tree(trait_tree_level):
 # generated the function calls itself to iterate this time through all of that traits subtraits, generating a random value for -->
 # the subtrait using the same random method as before. All of this ends up intuitively summing up from the lowest level upwards in chunks -->
 # resulting in a balanced trait presence percentage being reflected for every trait available.
-def iterate_value_generation(trait_tree, traits, subtraits = [], iteration = -1):
+def iterate_value_generation(trait_tree, traits, trait_tree_modifier = {}, subtraits = [], iteration = -1, name = 'null'):
     percentage_remaining = None
     selected_traits = None
 
@@ -141,10 +220,10 @@ def iterate_value_generation(trait_tree, traits, subtraits = [], iteration = -1)
 
         if len(subtraits) == 0:
             trait_tree['traits'][traits[i]]['value'] = float(random_percentage)/100
-            iterate_value_generation(trait_tree, traits, get_and_shuffle_trait_tree(trait_tree['traits'][traits[i]]['subtraits']), i)
+            iterate_value_generation(trait_tree, traits, trait_tree_modifier=trait_tree_modifier, subtraits=get_and_shuffle_trait_tree(trait_tree['traits'][traits[i]]['subtraits']), iteration=i, name=name)
         else:
             trait_tree['traits'][traits[iteration]]['subtraits'][subtraits[i]] = float(random_percentage)/100
 
         percentage_remaining = percentage_remaining - random_percentage
 
-    write_trait_tree(trait_tree)
+    trait_tree_modifier.write_trait_tree(trait_tree)
