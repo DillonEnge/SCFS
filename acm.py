2bf265c550f0441ea3f13cbafbb6c698501d61a3
jnshaw99 <jnshaw99@gmail.com>
2019-10-28 08:39:08 -0400

    adding new consonants

--- acm.py
+++ acm.py
@@ -18,6 +18,9 @@ alphabet={
         ['m', 'm'], 
         ['ɾ', 'r'], 
         ['s', 's'], 
+        ['st', 'st'], 
+        ['sp', 'sp'], 
+        ['sk', 'sk'], 
         ['z', 'z'], 
         ['ʃ', 'sh'], 
         ['ʒ', 'zh'], 
@@ -28,39 +31,6 @@ alphabet={
         ['d͡ʒ', 'j']
     ]
 }
-vocablist=[
-    ['ant', 'hardworking', 'forage'],
-    ['dog', 'loyal', 'pander'],
-    ['cat', 'independent', 'stalk'],
-    ['cow', 'dull', 'loiter'],
-    ['pig/boar', 'violent', 'trample'],
-    ['chicken', 'wide-eyed', 'chirp'],
-    ['bird','vibrant','fly'],
-    ['turkey','moody','extend/hold out'],
-    ['fish','foul-smelling','swim'],
-    ['turtle','secure/protected','protect'],
-    ['rabbit','fucund','multiple'],
-    ['human','humane/rational','reason/think about'],
-    ['sheep','furry/hairy','follow'],
-    ['goat','strong-willed','ram'],
-    ['camel','reliable','carry'],
-    ['llama','dramatic','spit'],
-    ['horse','working-class','carry'],
-    ['bug/insect','pesky/annoying','bug'],
-    ['here','this','return/receive'],
-    ['there 2nd person','that 2nd person','give'],
-    ['there 3rd person','that 3rd person','throw out/get rid of'],
-    ['i','my','flow/lost in thought'],
-    ['you','your','discuss'],
-    ['they/her/she','their','isolate'],
-    ['list','plural/many','count/list off'],
-    ['walk','close','walk'],
-    ['car','driving distance away','drive/guide'],
-    ['ship','far away','fly(plane)/drive(non-car)'],
-    ['sky','wide/spacious/open','rain'],
-    ['sun','sunny/bright','burn'],
-    ['moon/month','alone','record'],
-]
 with open('vocablist.json') as f:
     vocablist=json.load(f)
 dictionary={}
